59c066c16f527136f808aba5436ded8cde3209c0
David <david.samah@student.moringaschool.com>
2025-03-24 22:15:50 +0300

    submission

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+
+function saturdayFun(string="roller-skate"){
+
+    return `This Saturday, I want to ${string}!`
+}
+saturdayFun()
+
+const mondayWork = function(string="go to the office"){
+
+  return `This Monday, I will ${string}.`
+}
+mondayWork()
+
+function wrapAdjective(flair = "*") {
+    
+  
+    function createMessage(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  
+    return createMessage;
+  }
+  
+
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("amazing"));  
+  console.log(encouragingPromptFunction());  
+  
